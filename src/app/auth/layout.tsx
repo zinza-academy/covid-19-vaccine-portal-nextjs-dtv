8374bbcb354425ce1be6f31b28f9326eda2cd802
Dinhuty <93468153+dinhuty@users.noreply.github.com>
2024-04-12 10:21:11 +0700

    Issues 6 implement forgot password (#8)
    * refactor: Remove unnecessary files and code
    
    * feat(auth): implement forgot password

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,35 @@
+//styles
+import { Box } from '@mui/material';
+import Image from 'next/image';
+
+export default function AuthLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex'
+      }}
+    >
+      <Box
+        sx={{
+          flexBasis: '50%',
+          position: 'relative'
+        }}
+      >
+        <Image src={'/images/auth-background.png'} alt="auth bg" fill />
+      </Box>
+      <Box
+        sx={{
+          flexBasis: '50%'
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+}
